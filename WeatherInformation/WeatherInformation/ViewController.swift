a71b809aee0b912c111144389d57551f1a881d45
appleDevBooker <graysilver18@naver.com>
2022-06-24 15:06:24 +0900

    프로젝트 생성
    * iOS 14이상
    * 지원 화면 방향: 세로

---
+++ WeatherInformation/WeatherInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherInformation
+//
+//  Created by Eunsoo KIM on 2022/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
