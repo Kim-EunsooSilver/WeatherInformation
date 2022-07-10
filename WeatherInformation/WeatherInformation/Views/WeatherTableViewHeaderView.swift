4d32bc0f0c1cdc0867c14e889fb28d165054f6b6
appleDevBooker <graysilver18@naver.com>
2022-07-10 14:53:11 +0900

    feat: WeatherTableViewHeaderView 파일 생성

---
+++ WeatherInformation/WeatherInformation/Views/WeatherTableViewHeaderView.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherTableViewHeaderView.swift
+//  WeatherInformation
+//
+//  Created by Eunsoo KIM on 2022/07/08.
+//
+
+import UIKit
+
+class WeatherTableViewHeaderView: UITableViewHeaderFooterView {
+}
