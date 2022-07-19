e5d0abec831cd041ee213ef79995a5d669a7bef5
appleDevBooker <graysilver18@naver.com>
2022-07-19 11:43:09 +0900

    feat: WeatherCollectionViewCell 생성

---
+++ WeatherInformation/WeatherInformation/Views/WeatherCollectionViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherCollectionViewCell.swift
+//  WeatherInformation
+//
+//  Created by Eunsoo KIM on 2022/07/18.
+//
+
+import UIKit
+
+final class WeatherCollectionViewCell: UICollectionViewCell {
+}
