ecd69a2c5bfeec749b48ab0049fa7bd61abc5342
appleDevBooker <graysilver18@naver.com>
2022-07-19 11:43:40 +0900

    feat: UI Properties 선언

--- WeatherInformation/WeatherInformation/Views/WeatherCollectionViewCell.swift
+++ WeatherInformation/WeatherInformation/Views/WeatherCollectionViewCell.swift
@@ -8,4 +8,28 @@
 import UIKit
 
 final class WeatherCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - UI Properties
+
+    private let cityTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.textAlignment = .center
+        return label
+    }()
+    let weatherIcon: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    private let humidityLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
 }
