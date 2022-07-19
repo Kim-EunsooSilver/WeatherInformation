ab595043db9e86c8bafadcf889fa275872be988e
appleDevBooker <graysilver18@naver.com>
2022-07-19 12:07:25 +0900

    feat: WeatherCollectionReusableView 파일 생성 및 선언
    * 컬렉션뷰의 헤더뷰 역할을 하는 클래스

---
+++ WeatherInformation/WeatherInformation/Views/WeatherCollectionReusableView.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherCollectionReusableView.swift
+//  WeatherInformation
+//
+//  Created by Eunsoo KIM on 2022/07/19.
+//
+
+import UIKit
+
+final class WeatherCollectionReusableView: UICollectionReusableView {
+}
