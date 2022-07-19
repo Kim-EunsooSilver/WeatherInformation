ce09f27d6ce2a8c3ce6b722336a1dc0847d35783
appleDevBooker <graysilver18@naver.com>
2022-07-19 11:44:28 +0900

    feat: setLayout() 선언
    * UI Properties의 레이아웃을 설정하는 함수

--- WeatherInformation/WeatherInformation/Views/WeatherCollectionViewCell.swift
+++ WeatherInformation/WeatherInformation/Views/WeatherCollectionViewCell.swift
@@ -32,4 +32,39 @@ final class WeatherCollectionViewCell: UICollectionViewCell {
         label.adjustsFontSizeToFitWidth = true
         return label
     }()
+
+    // MARK: - setLayout
+
+    private func setLayout() {
+        contentView.addSubview(cityTitleLabel)
+        contentView.addSubview(weatherIcon)
+        contentView.addSubview(temperatureLabel)
+        contentView.addSubview(humidityLabel)
+        
+        cityTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        weatherIcon.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        humidityLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cityTitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            cityTitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            
+            weatherIcon.topAnchor.constraint(equalTo: cityTitleLabel.bottomAnchor, constant: 5),
+            weatherIcon.heightAnchor.constraint(equalToConstant: 100),
+            weatherIcon.widthAnchor.constraint(equalToConstant: 100),
+            weatherIcon.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            
+            temperatureLabel.topAnchor.constraint(equalTo: weatherIcon.bottomAnchor, constant: 5),
+            temperatureLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            
+            humidityLabel.topAnchor.constraint(equalTo: temperatureLabel.bottomAnchor, constant: 5),
+            humidityLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        ])
+        
+        self.contentView.layer.cornerRadius = 2.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+        self.contentView.layer.masksToBounds = true
+    }
 }
